25ae466b2c3e41d00f8e6aa61706a191e5ebe31c
Max Ovchinnikov <ovchinnicov132@gmail.com>
2021-12-29 21:06:03 +0300

    refactor: import variable configApi

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,10 +1,4 @@
-const configApi = {
-  url: "https://mesto.nomoreparties.co/v1/cohort-30/",
-  headers: {
-    authorization: "c5a5cb5f-db7e-4ac2-924a-a6deb50d8693",
-    "content-type": "application/json",
-  },
-};
+import { configApi } from "./Constants"
 
  class Api {
   constructor(config) {
