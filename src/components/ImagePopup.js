40bec5e0ebdcaa02e9cd9b67eafda504cb076721
Max Ovchinnikov <ovchinnicov132@gmail.com>
2021-12-29 20:49:59 +0300

    refactor: add variable className

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,20 +1,21 @@
-function ImagePopup(props) {
+function ImagePopup({card, onClose}) {
   
-const card = Boolean(JSON.stringify(props.card) ==='{}');
+const isCard = Boolean(JSON.stringify(card) ==='{}');
+const className = !isCard && "popup_opened";
 
     return(
-        <div className={`popup popup_type_image ${!card && "popup_opened"}`}>
+        <div className={`popup popup_type_image ${className}`}>
         <figure
           className="popup__container popup__container-for-image"
           tabIndex="0"
         >
-          <button className="popup__close" type="button" onClick={props.onClose}></button>
+          <button className="popup__close" type="button" onClick={onClose}></button>
           <img
             className="popup__element-image"
-            src={props.card.link}
-            alt={props.card.name}
+            src={card.link}
+            alt={card.name}
           />
-          <figcaption className="popup__image-caption">{props.card.name}</figcaption>
+          <figcaption className="popup__image-caption">{card.name}</figcaption>
         </figure>
       </div>
     )
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,19 +1,31 @@
 function PopupWithForm(props) {
-  
-return(
-    <div className={`popup popup_type_${props.name} ${props.isOpen && "popup_opened"}`}>
-    <div className="popup__container" tabIndex="0">
-      <button className="popup__close" type="button" onClick={props.onClose}></button>
-      <form name={props.name} className="form"  onSubmit={props.onSubmit} noValidate>
-        <fieldset className="form__input-container">
-          <h2 className="form__title">{props.title}</h2>
-          <>{props.children}</>
-        </fieldset>
-        <button className="form__button-save" type="submit">Сохранить</button>
-      </form>
+  const className = props.isOpen && "popup_opened";
+
+  return (
+    <div className={`popup ${className}`}>
+      <div className="popup__container" tabIndex="0">
+        <button
+          className="popup__close"
+          type="button"
+          onClick={props.onClose}
+        ></button>
+        <form
+          name={props.name}
+          className="form"
+          onSubmit={props.onSubmit}
+          noValidate
+        >
+          <fieldset className="form__input-container">
+            <h2 className="form__title">{props.title}</h2>
+            <>{props.children}</>
+          </fieldset>
+          <button className="form__button-save" type="submit">
+            Сохранить
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
-)
-} 
+  );
+}
 
 export default PopupWithForm;
